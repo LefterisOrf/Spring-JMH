e8de9ef1b24ff5b19f53ec02c88c39d5e68ab2d7
Lefteris Orfanidis <e.orfanidis@schoolbusnet.eu>
2023-09-03 23:57:30 +0300

    add: JMH runner

---
+++ src/test/java/com/example/demo/JMHRunner.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.io.IOException;
+
+public class JMHRunner {
+
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
