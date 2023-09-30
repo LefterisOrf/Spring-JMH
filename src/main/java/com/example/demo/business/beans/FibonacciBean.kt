ae117bd81b25551e5e1f0621560b22498ca9d7fa
Lefteris Orfanidis <e.orfanidis@schoolbusnet.eu>
2023-09-30 22:35:44 +0300

    fix: use concurrentHashmap instead of default kotlin's map

--- src/main/java/com/example/demo/business/beans/FibonacciBean.kt
+++ src/main/java/com/example/demo/business/beans/FibonacciBean.kt
@@ -3,45 +3,55 @@ package com.example.demo.business.beans
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
 
 @Service
 class FibonacciBean {
-    private val logger: Logger = LoggerFactory.getLogger(FibonacciBean::class.java);
+    private val logger: Logger = LoggerFactory.getLogger(FibonacciBean::class.java)
 
-    private val values:MutableMap<Int, Long> = hashMapOf(Pair(0, 0), Pair(1, 1));
+    private val values:ConcurrentHashMap<Int, Long> = ConcurrentHashMap()
 
     fun listValues() : Collection<Long> {
-        return values.values;
+        return values.values
     }
 
     fun recursive( value: Int):Long {
         if (value < 0)
-            throw IllegalArgumentException("Must be >= 0");
+            throw IllegalArgumentException("Must be >= 0")
 
         if (value == 1)
-            return 1;
+            return 1
 
         if (value == 0)
-            return 0;
+            return 0
 
-        return recursive( value - 1) + recursive( value - 2);
+        return recursive( value - 1) + recursive( value - 2)
     }
 
     fun dynamic( value: Int ):Long {
 
         if (value < 0)
-            throw IllegalArgumentException("Must be >= 0");
+            throw IllegalArgumentException("Must be >= 0")
 
         if (values.containsKey(value)) {
-            logger.debug("Found value $value in dictionary.");
-            return values[value]!!;
+            logger.debug("Found value $value in dictionary.")
+            return values[value]!!
         }
 
+        if ( value == 0) {
+            values[0] = 0
+            return 0
+        }
 
-        val result = dynamic(value - 1) + dynamic( value - 2);
-        logger.debug("Adding $value and $result to dictionary.");
-        values[value] = result;
-        return result;
+        if (value == 1) {
+            values[1] = 1
+            return 1
+        }
+
+        val result = dynamic(value - 1) + dynamic( value - 2)
+        logger.debug("Adding $value and $result to dictionary.")
+        values[value] = result
+        return result
     }
 
 }
